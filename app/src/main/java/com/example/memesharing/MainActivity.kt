757ca39f2d8ed21418c3d81b5cc71e797b1c94ca
Sapna2001 <harisapnanair@gmail.com>
2020-09-16 22:41:14 +0530

    files added

---
+++ app/src/main/java/com/example/memesharing/MainActivity.kt
@@ -0,0 +1,21 @@
+package com.example.memesharing
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+
+    fun shareMeme(view: View) {
+
+    }
+    fun nextMeme(view: View) {
+
+    }
+}
+
+// api -> https://meme-api.herokuapp.com/gimme
