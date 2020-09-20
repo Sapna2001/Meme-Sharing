989d303376fc6962d56976b37684ca7485f26c71
Sapna2001 <harisapnanair@gmail.com>
2020-09-20 21:58:14 +0530

    Files added

--- app/src/main/java/com/example/memesharing/MainActivity.kt
+++ app/src/main/java/com/example/memesharing/MainActivity.kt
@@ -1,18 +1,28 @@
 package com.example.memesharing
 
+import android.content.Intent
+import android.graphics.drawable.Drawable
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import android.view.View
+import android.widget.Toast
 import com.android.volley.Request
 import com.android.volley.Response
 import com.android.volley.toolbox.JsonObjectRequest
 import com.android.volley.toolbox.StringRequest
 import com.android.volley.toolbox.Volley
 import com.bumptech.glide.Glide
+import com.bumptech.glide.load.DataSource
+import com.bumptech.glide.load.engine.GlideException
+import com.bumptech.glide.request.RequestListener
+import com.bumptech.glide.request.target.Target
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
+    var currentUrl : String ? = null;
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -20,9 +30,6 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun loadMeme() {
-        // Instantiate the RequestQueue.
-        val queue = Volley.newRequestQueue(this)
-
 //        val url = "https://www.google.com"
 //
 //        // Request a string response from the provided URL.
@@ -35,27 +42,57 @@ class MainActivity : AppCompatActivity() {
 //                Log.d("Error",it.localizedMessage)
 //            })
 
-        val url = "https://meme-api.herokuapp.com/gimme"
+        progress_bar.visibility = View.VISIBLE;
+        currentUrl = "https://meme-api.herokuapp.com/gimme"
 
-        val jsonObjectRequest = JsonObjectRequest(Request.Method.GET, url, null,
+        val jsonObjectRequest = JsonObjectRequest(Request.Method.GET, currentUrl, null,
             Response.Listener { response ->
                 val url = response.getString("url");
-                Glide.with(this).load(url).into(memeImageView)
+
+                Glide.with(this).load(url).listener(object : RequestListener<Drawable>{
+
+                    override fun onLoadFailed(
+                        e: GlideException?,
+                        model: Any?,
+                        target: Target<Drawable>?,
+                        isFirstResource: Boolean
+                    ): Boolean {
+                        return false
+                    }
+
+                    override fun onResourceReady(
+                        resource: Drawable?,
+                        model: Any?,
+                        target: Target<Drawable>?,
+                        dataSource: DataSource?,
+                        isFirstResource: Boolean
+                    ): Boolean {
+                        progress_bar.visibility = View.GONE;
+                        return false;
+                    }
+
+                }).into(memeImageView)
             },
             Response.ErrorListener { error ->
-                // TODO: Handle error
+                progress_bar.visibility = View.GONE
+                Toast.makeText(this, "Something went wrong", Toast.LENGTH_LONG).show()
             }
         )
 
         // Add the request to the RequestQueue.
-        queue.add(jsonObjectRequest)
+        MySingleton.getInstance(this).addToRequestQueue(jsonObjectRequest);
     }
 
     fun shareMeme(view: View) {
-
+        val intent = Intent(Intent.ACTION_SEND);
+        intent.type = "text/plain";
+        intent.putExtra(Intent.EXTRA_TEXT,"Hi, checkout this meme $currentUrl");
+        val chooser = Intent.createChooser(intent, "Share this meme using ...");
+        startActivity(chooser);
     }
-    fun nextMeme(view: View) {
 
+    fun nextMeme(view: View) {
+        loadMeme();
     }
 }
 
